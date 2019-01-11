fb281d390802917855ac71060484c00116e1492a
accuracycpa <46612162+accuracycpa@users.noreply.github.com>
2019-01-11 16:53:00 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
 window.onload = function() {
     document.getElementById('bodycontent').className = 'loaded';
-    var loaderFade = document.getElementByid('loader-wrapper')
-    loaderFade.classList.toggle('fade');
 };
