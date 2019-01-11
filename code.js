58c56494d737406710f64ece03886d1a5f963b4e
accuracycpa <46612162+accuracycpa@users.noreply.github.com>
2019-01-11 16:56:59 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
-window.onload = function() {
+/*window.onload = function() {
     document.getElementById('bodycontent').className = 'loaded';
-};
+};*/
+
+setTimeout(function() {
+    document.getElementById('bodycontent').className = 'loaded';
+}, 5000);
